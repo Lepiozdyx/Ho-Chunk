b13cc22788d8164adba103a76fb7cd0fa6e4b5dc
Developer <Lepiozdyx@gmail.com>
2025-04-18 10:32:46 -0300

    Initial Commit

---
+++ Ho-Chunk/Ho_ChunkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ho_ChunkApp.swift
+//  Ho-Chunk
+//
+//  Created by Alex on 18.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct Ho_ChunkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
